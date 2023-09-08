008bdaa7d763766edb22d9de958f7ef35da2e886
Manu Kapolke <manukapolke@gmail.com>
2023-09-08 11:15:29 +0200

    added firebase, defined collection ref functions

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  firestore: Firestore = inject(Firestore);
+
   constructor() { }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dakeep-ff1b6',
+    appId: '1:161458516696:web:1b27a028a0b1142cf95dad',
+    storageBucket: 'dakeep-ff1b6.appspot.com',
+    apiKey: 'AIzaSyCl5-mlbxb28HXVXnjoC2xmtwy194GIFrg',
+    authDomain: 'dakeep-ff1b6.firebaseapp.com',
+    messagingSenderId: '161458516696',
+  },
+};
